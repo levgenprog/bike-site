219560aae733fd9513ebaf82f37823add7c294ef
levgen <levgenprog@ya.ru>
2023-04-25 14:42:21 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from "react";
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <h2>апыфв</h2>
+      <h3>sgsdgadasg</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum commodi, nostrum quod in, magni inventore assumenda voluptatem beatae repellat, vel modi atque! Aperiam quaerat incidunt, recusandae non quidem minus quod.</p>
+    </>
+  );
+}
+
+export default App;
